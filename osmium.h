93e844da5ec44dffd9081748deff053322c00423
Siqing Zhang <siqing.zhang@student.unsw.edu.au>
2023-11-26 22:27:08 +1100

    include

--- osmium.h
+++ osmium.h
@@ -6,6 +6,7 @@
 
 #endif //STREETMATCH_OSMIUM_H
 
+#include <osmium/osm.hpp>
 #include <osmium/io/reader.hpp>
 #include <osmium/io/writer.hpp>
 #include <osmium/handler.hpp>
@@ -20,3 +21,7 @@
 #include <osmium/geom/projection.hpp>
 #include <osmium/geom/coordinates.hpp>
 #include <osmium/geom/factory.hpp>
+#include <osmium/geom/relations.hpp>
+#include <osmium/io/any_input.hpp>
+#include <osmium/io/any_output.hpp>
+#include <osmium/io/any_compression.hpp>
